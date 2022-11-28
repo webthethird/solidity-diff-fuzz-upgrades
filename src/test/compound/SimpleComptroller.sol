0ededb2ded3f51de7596f9d58aa100fe3ff0d384
webthethird <ratzbodell@gmail.com>
2022-11-28 07:01:35 -0600

    Trim the fat Remove references to borrower markets Fix typo

--- src/test/compound/SimpleComptroller.sol
+++ src/test/compound/SimpleComptroller.sol
@@ -22,13 +22,11 @@ contract SimpleComptroller {
     Token constant COMP = Token(0xc00e94Cb662C3520282E6f5717214004A7f26888);
     uint224 public constant compInitialAccruedPerUnit = 1e36;
     mapping(address => CompMarketState) public compSupplyState;
-    mapping(address => CompMarketState) public compBorrowState;
     mapping(address => mapping(address => uint)) public compSupplierAccruedPerUnit;
-    mapping(address => mapping(address => uint)) public compBorrowerAccruedPerUnit;
     mapping(address => uint) public compAccrued;
     mapping(address => uint) public compSpeeds;
 /*-----------------------------------snip-------------------------------------*/
-    function distributeSupplierComp(address cToken, address supplier) internal {  // Matches distributeBorrowerComp()
+    function distributeSupplierComp(address cToken, address supplier) internal {
         CompMarketState storage supplyState = compSupplyState[cToken];
         uint supplyAccruedPerUnit = supplyState.compAccruedPerUnit;
         uint supplierAccruedPerUnit = compSupplierAccruedPerUnit[cToken][supplier];
@@ -40,31 +38,25 @@ contract SimpleComptroller {
             // Rewards user with COMP accrued from when supplier rewards were first set for the market.
             supplierAccruedPerUnit = compInitialAccruedPerUnit;
         }
-        uint deltaAccruedPerUnit = supplyAccruedPerUnit - supplierAccruedPerUnit;
+        uint delta = supplyAccruedPerUnit - supplierAccruedPerUnit;
         uint supplierTokens = Token(cToken).balanceOf(supplier);
-        uint supplierDelta = supplierTokens * deltaAccruedPerUnit;
+        uint supplierDelta = supplierTokens * delta;
         compAccrued[supplier] = compAccrued[supplier] + supplierDelta;
     }
 /*-----------------------------------snip-------------------------------------*/
-    function updateCompSupplyAccrued(address cToken) internal {  // Matches updateCompBorrowAccrued()
+    function updateCompSupplyAccrued(address cToken) internal {
         CompMarketState storage supplyState = compSupplyState[cToken];
         uint deltaBlocks = block.number - uint(supplyState.block);
         uint compAccrued = deltaBlocks * compSpeeds[cToken];
         uint ratio = compAccrued / Token(cToken).totalSupply();
-        uint index = supplyState.index + ratio;
-        compSupplyState[cToken] = CompMarketState({
-            index: index,
-            block: block.number
-        });
+        uint index = supplyState.compAccruedPerUnit + ratio;
+        compSupplyState[cToken] = CompMarketState({ index: index, block: block.number });
     }
 /*-----------------------------------snip-------------------------------------*/
     function claimComp(Token[] memory cTokens) public {
         for (uint i = 0; i < cTokens.length; i++) {
             Token cToken = cTokens[i];
-            require(markets[address(cToken)].isListed, "market must be listed");
-            updateCompBorrowAccrued(address(cToken));
             updateCompSupplyAccrued(address(cToken));
-            distributeBorrowerComp(address(cToken), msg.sender);
             distributeSupplierComp(address(cToken), msg.sender);
         }
         compAccrued[msg.sender] = grantComp(msg.sender, compAccrued[msg.sender]);
@@ -80,16 +72,10 @@ contract SimpleComptroller {
 /*---------------------------------new code-----------------------------------*/
     function _initializeMarket(address cToken) internal {
         CompMarketState storage supplyState = compSupplyState[cToken];
-        CompMarketState storage borrowState = compBorrowState[cToken];
         if (supplyState.compAccruedPerUnit == 0) {
             // Initialize supply state compAccruedPerUnit with default value
             supplyState.compAccruedPerUnit = compInitialAccruedPerUnit;
         }
-        if (borrowState.compAccruedPerUnit == 0) {
-            // Initialize borrow state compAccruedPerUnit with default value
-            borrowState.compAccruedPerUnit = compInitialAccruedPerUnit;
-        }
-         supplyState.block = borrowState.block = block.number;
+         supplyState.block = block.number;
     }
 }
-
