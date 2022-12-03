06386584f23e90f2d8047abc5f494cd2c8c17712
webthethird <ratzbodell@gmail.com>
2022-12-02 22:18:18 -0600

    Fuzz on index to an external list of addresses. Read addresses as strings in compound_accounts.txt using vm.readLine, then convert string to address. Works now that retry detector is updated.

--- src/test/compound/Comptroller-fork-diff.sol
+++ src/test/compound/Comptroller-fork-diff.sol
@@ -43,14 +43,42 @@ contract TestComptroller is Test {
     uint256 after_fork_id;
     string rpc;
 
+    string constant users_file = "/home/webthethird/Ethereum/solidity-diff-fuzz-upgrades/compound_accounts.txt";
+    uint num_users;
+    address[] users;
+
     function setUp() public {
         rpc = vm.envString("RPC_URL");
         before_fork_id = vm.createFork(rpc, before_block);
         after_fork_id = vm.createFork(rpc, after_block);
+        num_users = st2num(vm.readLine(users_file));
+        users = new address[](num_users);
+        console.logString("Number of addresses:");
+        console.logUint(num_users);
+        for(uint i = 0; i < num_users; i++) {
+            address user_addr = address(uint160(st2num(vm.readLine(users_file))));
+            // console.log(i, user_addr);
+            users[i] = user_addr;
+        }
+        vm.closeFile(users_file);
+    }
+
+    function st2num(string memory numString) internal pure returns(uint) {
+        uint  val=0;
+        bytes memory stringBytes = bytes(numString);
+        for (uint  i =  0; i<stringBytes.length; i++) {
+            uint exp = stringBytes.length - i;
+            bytes1 ival = stringBytes[i];
+            uint8 uval = uint8(ival);
+            uint jval = uval - uint(0x30);
+   
+            val +=  (uint(jval) * (10**(exp-1))); 
+        }
+        return val;
     }
 
     /**
-     * Test claiming COMP on both forks and assert that the amount of tokens transfered is the same.
+     * Test claiming COMP on both forks and assert that the amount of tokens transfered is roughly the same.
      * 
      * Ideally the gap in block numbers should be minimal to ensure any difference is not due to state
      * changes between blocks, or just more accrual over time (need to double-check how accruals work).
@@ -58,12 +86,16 @@ contract TestComptroller is Test {
      * Right now the address is what the fuzzer will mutate, but it would be better to have a
      * pre-defined list of known participant addresses to choose randomly from.
      */
-    function test_claimComp_diff_before_after(address holder) public {
+    function test_claimComp_diff_before_after(uint16 _index) public {
+        vm.assume(_index < num_users);
+        address holder = users[_index];
+        console.log(_index, holder);
+
         vm.selectFork(before_fork_id);
 
         // Disregard test run if the given address is not a participant in any Compound markets
-        CToken[] memory assetsIn = unitroller.getAssetsIn(holder);
-        vm.assume(assetsIn.length > 0);
+        // CToken[] memory assetsIn = unitroller.getAssetsIn(holder);
+        // vm.assume(assetsIn.length > 0);
 
         // Check the COMP balance of the holder before and after claiming from the old Comptroller
         Multicall2.Call[] memory calls = new Multicall2.Call[](3);
@@ -77,14 +109,14 @@ contract TestComptroller is Test {
         calls[2].target = address(comp);
         calls[2].callData = abi.encodeWithSelector(comp.balanceOf.selector, holder);
         
-        Multicall2.Result[] memory results_before = Multicall2(0x5BA1e12693Dc8F9c48aAD8770482f4739bEeD696).tryAggregate(false, calls);
+        Multicall2.Result[] memory results_before = Multicall2(0x5BA1e12693Dc8F9c48aAD8770482f4739bEeD696).tryAggregate(true, calls);
         uint256 balance_before = abi.decode(results_before[0].returnData, (uint256));
         uint256 new_balance_before = abi.decode(results_before[2].returnData, (uint256));
         uint256 delta_before = new_balance_before - balance_before;
 
         // Switch to the fork from after the upgrade, then perform the same calls
         vm.selectFork(after_fork_id);
-        Multicall2.Result[] memory results_after = Multicall2(0x5BA1e12693Dc8F9c48aAD8770482f4739bEeD696).tryAggregate(false, calls);
+        Multicall2.Result[] memory results_after = Multicall2(0x5BA1e12693Dc8F9c48aAD8770482f4739bEeD696).tryAggregate(true, calls);
         uint256 balance_after = abi.decode(results_after[0].returnData, (uint256));
         uint256 new_balance_after = abi.decode(results_after[2].returnData, (uint256));
         uint256 delta_after = new_balance_after - balance_after;
