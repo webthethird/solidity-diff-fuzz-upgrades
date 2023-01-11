8bcd3fa9e6e2ac124de293201ba488fd362bfb1b
webthethird <ratzbodell@gmail.com>
2023-01-11 10:26:17 -0600

    Add simple contracts to test HEVM store cheat code

---
+++ src/test/example/testStore.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "./Smartian_motiv.sol";
+import "../helpers.sol";
+
+contract TestStore {
+    address admin;
+    C c;
+
+    constructor() {
+        admin = msg.sender;
+    }
+
+    function testStore() public {
+        c = new C();
+        require(c.owner() == address(this));
+        CheatCodes(HEVM_ADDRESS).store(address(c), bytes32(0), bytes32(bytes20(admin)));
+        assert(address(bytes20(CheatCodes(HEVM_ADDRESS).load(address(c), bytes32(0)))) == admin);
+        assert(c.owner() == admin);
+    }
+}
