c293dd24ee4f5cfc85415b12ceec950a7742a69e
webthethird <ratzbodell@gmail.com>
2023-01-16 14:33:13 -0600

    Update build script to send COMP to Faucet and Reservoir

--- build.sh
+++ build.sh
@@ -96,6 +96,13 @@ DEPLOY_COMPOUND_WITH_SUFFIX () {
     FAUCET_ADDRESS=$(cat ./compound-eureka/networks/development-${SUFFIX}.json | python3 -c "import sys, json; print(json.loads(sys.stdin.read())['Contracts']['Fauceteer'])")
     FAUCET_ADDRESS=${FAUCET_ADDRESS#0x}
     echo "address constant FAUCET_${SUFFIX}_ADDR = address(0x00$FAUCET_ADDRESS);" >> /tmp/addresses.sol.tmp
+    RESERVOIR_ADDRESS=$(cat ./compound-eureka/networks/development-${SUFFIX}.json | python3 -c "import sys, json; print(json.loads(sys.stdin.read())['Contracts']['Reservoir'])")
+    RESERVOIR_ADDRESS=${RESERVOIR_ADDRESS#0x}
+    echo "address constant RESERVOIR_${SUFFIX}_ADDR = address(0x00$RESERVOIR_ADDRESS);" >> /tmp/addresses.sol.tmp
+    COMP_ADDRESS=$(cat ./compound-eureka/networks/development-${SUFFIX}.json | python3 -c "import sys, json; print(json.loads(sys.stdin.read())['Contracts']['Comp'])")
+    echo "# Transferring COMP to faucet and reservoir..."
+    cast send --legacy --private-key $GANACHE_KEY $COMP_ADDRESS "transfer(address,uint256)" 0x$FAUCET_ADDRESS 1000000000000000000000000
+    cast send --legacy --private-key $GANACHE_KEY $COMP_ADDRESS "transfer(address,uint256)" 0x$RESERVOIR_ADDRESS 4000000000000000000000000
 }
 UPGRADE_COMPOUND_WITH_VERSION_KEY () {
     local NETWORK_FILE=$1
