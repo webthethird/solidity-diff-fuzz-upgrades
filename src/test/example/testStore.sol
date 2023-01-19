3f2f12ea82dc34044ef1bb0bfed6d7295ed67b18
webthethird <ratzbodell@gmail.com>
2023-01-19 13:04:43 -0600

    Fix testStore.sol

--- src/test/example/testStore.sol
+++ src/test/example/testStore.sol
@@ -13,26 +13,42 @@ contract C {
 }
 
 contract TestStore {
+    event AdminIs(address admin);
+    event OwnerIs(address owner);
+    event LoadedValue(bytes32 val);
+
     address admin;
     C c;
 
     constructor() {
         admin = msg.sender;
+        emit AdminIs(admin);
     }
 
     function testStore() public {
         c = new C();
         require(c.owner() == address(this));
+        emit OwnerIs(c.owner());
+
+        bytes32 loadedValue = CheatCodes(HEVM_ADDRESS).load(
+            address(c),
+            bytes32(0)
+        );
+        emit LoadedValue(loadedValue);
+
+        emit AdminIs(admin);
         CheatCodes(HEVM_ADDRESS).store(
             address(c),
             bytes32(0),
-            bytes32(bytes20(admin))
+            bytes32(uint256(uint160(admin)))
         );
-        assert(
-            address(
-                bytes20(CheatCodes(HEVM_ADDRESS).load(address(c), bytes32(0)))
-            ) == admin
-        );
+
+        loadedValue = CheatCodes(HEVM_ADDRESS).load(address(c), bytes32(0));
+        emit LoadedValue(loadedValue);
+
+        assert(loadedValue == bytes32(uint256(uint160(admin))));
+
+        emit OwnerIs(c.owner());
         assert(c.owner() == admin);
     }
 }
