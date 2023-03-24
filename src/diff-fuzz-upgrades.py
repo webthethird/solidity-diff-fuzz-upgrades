f8d020e6fc44ee5eb31943e0715cd50b0aa33de7
webthethird <ratzbodell@gmail.com>
2023-03-24 15:46:59 -0500

    Fix handling of tainted variables with proxy

--- src/diff-fuzz-upgrades.py
+++ src/diff-fuzz-upgrades.py
@@ -563,6 +563,11 @@ def wrap_diff_functions(v1, v2, proxy=None):
 
     wrapped += "\n    /*** Tainted Variables ***/ \n\n"
     for v in diff["tainted-variables"]:
+        if proxy is None:
+            target_v1 = camel_case(v1['name'])
+            target_v2 = camel_case(v2['name'])
+        else:
+            target_v1 = target_v2 = camel_case(proxy['name'])
         if v.visibility in ["internal", "private"]:
             continue
         if v.type.is_dynamic:
@@ -571,15 +576,15 @@ def wrap_diff_functions(v1, v2, proxy=None):
                 wrapped += (
                     f"    function {v1['name']}_{v.name}({type_from} a) public {{\n"
                 )
-                wrapped += f"        assert({camel_case(v1['name'])}V1.{v.name}(a) == {camel_case(v2['name'])}V2.{v.name}(a));\n"
+                wrapped += f"        assert({target_v1}V1.{v.name}(a) == {target_v2}V2.{v.name}(a));\n"
                 wrapped += "    }\n\n"
             elif isinstance(v.type, ArrayType):
                 wrapped += f"    function {v1['name']}_{v.name}(uint i) public {{\n"
-                wrapped += f"        assert({camel_case(v1['name'])}V1.{v.name}(i) == {camel_case(v2['name'])}V2.{v.name}(i));\n"
+                wrapped += f"        assert({target_v1}V1.{v.name}(i) == {target_v2}V2.{v.name}(i));\n"
                 wrapped += "    }\n\n"
         else:
             wrapped += f"    function {v1['name']}_{v.full_name} public {{\n"
-            wrapped += f"        assert({camel_case(v1['name'])}V1.{v.full_name} == {camel_case(v2['name'])}V2.{v.full_name});\n"
+            wrapped += f"        assert({target_v1}V1.{v.full_name} == {target_v2}V2.{v.full_name});\n"
             wrapped += "    }\n\n"
 
     return wrapped
