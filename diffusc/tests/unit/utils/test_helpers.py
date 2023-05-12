c6e9ce76152fe292ed93c646e157395be28edcee
webthethird <ratzbodell@gmail.com>
2023-05-12 11:09:55 -0500

    Unit test for `get_pragma_versions_from_file`

--- diffusc/tests/unit/utils/test_helpers.py
+++ diffusc/tests/unit/utils/test_helpers.py
@@ -3,9 +3,24 @@ from pathlib import Path
 
 from solc_select import solc_select
 from slither import Slither
+from diffusc.utils import helpers
 
 TEST_DATA_DIR = Path(__file__).resolve().parent / "test_data" / "helpers"
 
 
 def test_pragma_from_file() -> None:
-    pass
+    file = Path(TEST_DATA_DIR, "pragmaA.sol").as_posix()
+    versions = helpers.get_pragma_versions_from_file(file)
+    assert versions == ("0.8.0", "0.8.20")
+    file = Path(TEST_DATA_DIR, "pragmaB.sol").as_posix()
+    versions = helpers.get_pragma_versions_from_file(file)
+    assert versions == ("0.8.2", "0.8.20")
+    file = Path(TEST_DATA_DIR, "pragmaC.sol").as_posix()
+    versions = helpers.get_pragma_versions_from_file(file)
+    assert versions == ("0.8.2", "0.8.17")
+    file = Path(TEST_DATA_DIR, "pragmaD.sol").as_posix()
+    versions = helpers.get_pragma_versions_from_file(file)
+    assert versions == ("0.8.2", "0.8.14")
+    file = Path(TEST_DATA_DIR, "pragmaE.sol").as_posix()
+    versions = helpers.get_pragma_versions_from_file(file)
+    assert versions == ("0.8.10", "0.8.10")
