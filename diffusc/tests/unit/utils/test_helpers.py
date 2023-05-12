ebc4773eb494202d7aa91e0d4e57ea50a5aa3115
webthethird <ratzbodell@gmail.com>
2023-05-12 12:07:24 -0500

    Fill in remaining helper tests to do later

--- diffusc/tests/unit/utils/test_helpers.py
+++ diffusc/tests/unit/utils/test_helpers.py
@@ -24,3 +24,21 @@ def test_pragma_from_file() -> None:
     file = Path(TEST_DATA_DIR, "pragmaE.sol").as_posix()
     versions = helpers.get_pragma_versions_from_file(file)
     assert versions == ("0.8.10", "0.8.10")
+
+
+def test_compilation_unit_name() -> None:
+    file = Path(TEST_DATA_DIR, "pragmaA.sol").as_posix()
+    sl = Slither(file)
+    assert helpers.get_compilation_unit_name(sl) == "pragmaA"
+
+
+def test_do_diff() -> None:
+    pass
+
+
+def test_similar() -> None:
+    pass
+
+
+def test_camel_case() -> None:
+    pass
