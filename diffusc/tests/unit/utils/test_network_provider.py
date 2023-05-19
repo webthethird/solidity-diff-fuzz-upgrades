5b749f1776d9fba7063b44f47b6f1127e0d16604
webthethird <ratzbodell@gmail.com>
2023-05-19 12:46:13 -0500

    Pylint, black, mypy

--- diffusc/tests/unit/utils/test_network_provider.py
+++ diffusc/tests/unit/utils/test_network_provider.py
@@ -1,11 +1,12 @@
 import os
 from pathlib import Path
 
-import pytest
+# import pytest
 from solc_select.solc_select import switch_global_version
 from slither import Slither
 from diffusc.utils.network_info_provider import NetworkInfoProvider
 from diffusc.core.code_generation import CodeGenerator
+
 TEST_DATA_DIR = Path(__file__).resolve().parent / "test_data"
 
 
@@ -22,33 +23,41 @@ def test_bad_rpc_init() -> None:
 def test_bad_poa_init() -> None:
     # Should fail because is_poa = True should be passed into NetworkInfoProvider for BSC
     rpc_url = os.getenv("BSC_RPC_URL")
+    assert rpc_url is not None
     try:
         NetworkInfoProvider(rpc_url, "latest")
         assert False
     except ValueError as err:
-        assert str(err) == "Got ExtraDataLengthError when getting block latest. " \
-                           "Probably missing network value, if RPC url is for a POA chain."
+        assert (
+            str(err) == "Got ExtraDataLengthError when getting block latest. "
+            "Probably missing network value, if RPC url is for a POA chain."
+        )
 
 
 def test_bad_block_init() -> None:
     # Should fail because "final" is not a valid block identifier (should be "finalized")
     rpc_url = os.getenv("BSC_RPC_URL")
+    assert rpc_url is not None
     try:
         NetworkInfoProvider(rpc_url, "final", is_poa=True)
         assert False
     except ValueError as err:
-        assert str(err) == '"final" is not a valid block identifier. Use "latest", "earliest", ' \
-                           '"pending", "safe" or "finalized" if not specifying an integer block number'
+        assert (
+            str(err) == '"final" is not a valid block identifier. Use "latest", "earliest", '
+            '"pending", "safe" or "finalized" if not specifying an integer block number'
+        )
 
 
 def test_block_timestamp() -> None:
     rpc_url = os.getenv("BSC_RPC_URL")
+    assert rpc_url is not None
     net_info = NetworkInfoProvider(rpc_url, 26857408, is_poa=True)
     assert net_info.get_block_timestamp() == 1680008936
 
 
 def test_block_number() -> None:
     rpc_url = os.getenv("BSC_RPC_URL")
+    assert rpc_url is not None
     net_info = NetworkInfoProvider(rpc_url, 26857408, is_poa=True)
     assert net_info.get_block_timestamp() == 26857408
     net_info = NetworkInfoProvider(rpc_url, 0, is_poa=True)
@@ -57,6 +66,7 @@ def test_block_number() -> None:
 
 def test_contract_variable_value() -> None:
     rpc_url = os.getenv("GOERLI_RPC_URL")
+    assert rpc_url is not None
     net_info = NetworkInfoProvider(rpc_url, "latest")
     api_key = os.getenv("GOERLI_API_KEY")
     contract_addr = "0xDc0Da9E56d7AEaA47b0f4913bAbb467b6E0C81cB"
@@ -73,6 +83,7 @@ def test_contract_variable_value() -> None:
 def test_empty_proxy_implementation() -> None:
     # Should fail because the BadProxy I deployed has no address stored in _IMPLEMENTATION_SLOT
     rpc_url = os.getenv("GOERLI_RPC_URL")
+    assert rpc_url is not None
     net_info = NetworkInfoProvider(rpc_url, "latest")
     api_key = os.getenv("GOERLI_API_KEY")
     contract_addr = "0xDc0Da9E56d7AEaA47b0f4913bAbb467b6E0C81cB"
@@ -91,6 +102,7 @@ def test_empty_proxy_implementation() -> None:
 def test_proxy_missing_slot_info() -> None:
     # Should fail because the BadProxy I deployed overrides the fallback and breaks the data dependency
     rpc_url = os.getenv("GOERLI_RPC_URL")
+    assert rpc_url is not None
     net_info = NetworkInfoProvider(rpc_url, "latest")
     api_key = os.getenv("GOERLI_API_KEY")
     contract_addr = "0x5a763c928430bc5742A144358B68CD8E14243030"
@@ -107,6 +119,7 @@ def test_proxy_missing_slot_info() -> None:
 
 def test_missing_token_holders() -> None:
     rpc_url = os.getenv("GOERLI_RPC_URL")
+    assert rpc_url is not None
     net_info = NetworkInfoProvider(rpc_url, "latest")
     api_key = os.getenv("GOERLI_API_KEY")
     contract_addr = "0xDc0Da9E56d7AEaA47b0f4913bAbb467b6E0C81cB"
@@ -119,5 +132,7 @@ def test_missing_token_holders() -> None:
     try:
         net_info.get_token_holders(1000, 1, contract_addr, abi)
     except ValueError as err:
-        assert str(err) == f"Contract at {contract_addr} doesn't appear to be a token. " \
-                           "It does not have a Transfer event."
+        assert (
+            str(err) == f"Contract at {contract_addr} doesn't appear to be a token. "
+            "It does not have a Transfer event."
+        )
