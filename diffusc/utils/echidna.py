b64a3cb389880b775209b9a2f4956b0f640b901d
webthethird <ratzbodell@gmail.com>
2023-05-22 09:00:01 -0500

    Print all stderr output from echidna

--- diffusc/utils/echidna.py
+++ diffusc/utils/echidna.py
@@ -54,6 +54,21 @@ def run_echidna_campaign(proc: Popen, min_tests: int = 1) -> int:
                     keep_running = (
                         False  # Useful for quick CI tests, but it will be removed in production
                     )
+    if max_value == -1:
+        # Print Echidna error output before returning
+        try:
+            assert proc.stderr is not None
+            line = proc.stderr.readline()
+            print(line.strip())
+        except UnsupportedOperation:
+            line = ""
+        while line != "":
+            try:
+                line = proc.stderr.readline()
+                print(line.strip())
+            except UnsupportedOperation:
+                line = ""
+
     CryticPrint.print_information("* Terminating Echidna campaign!")
     proc.terminate()
     proc.wait()
